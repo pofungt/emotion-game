61fa2591eca970e222f8975c0967c122d9d198d6
oneone105 <one_samuel@hotmail.com>
2022-11-03 22:26:51 +0000

    express

--- node-js/main.ts
+++ node-js/main.ts
@@ -1,2 +1,23 @@
 import dotenv from "dotenv";
 dotenv.config();
+
+import express from 'express';
+import http from "http";
+import path from "path";
+
+
+const app = express();
+const server = new http.Server(app)
+
+app.use(express.static('./public'));
+
+app.use((req, res) => {
+    res.status(404);
+    res.sendFile(path.resolve('./public404.html'));
+});
+
+const PORT = 8080;
+
+server.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`)
+})
