5c7ee1d6c5cbca40502d87e79a3153e9bc0c6413
Duncan Tang <pofungt@hotmail.com>
2022-11-04 18:02:36 -0400

    Done stream data to server and obtain prediction

--- node-js/public/index.js
+++ node-js/public/index.js
@@ -23,16 +23,37 @@ function openCvReady() {
 
         const FPS = 30;
         
-        function processVideo() {
+        async function processVideo() {
             let begin = Date.now();
             cap.read(src);
             src.copyTo(dst);
             cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
+            frame64 = getFrame()
+            const res = await fetch('http://localhost:8000/stream',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({img: frame64})
+            })
+            prediction = await res.json();
+            console.log(prediction)
+
             cv.imshow("canvasOutput", dst);
             // schedule next one.
             let delay = 1000/FPS - (Date.now() - begin);
             setTimeout(processVideo, delay);
         }
+
+        function getFrame() {
+            const canvas = document.createElement('canvas');
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            canvas.getContext('2d').drawImage(video, 0, 0);
+            const data = canvas.toDataURL('image/jpeg');
+            return data;
+        }
+
         // schedule first one.
         setTimeout(processVideo, 0);
     };
