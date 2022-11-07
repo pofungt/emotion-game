9588142a8206b85677f43cccae20ae86dc817d42
oneone105 <one_samuel@hotmail.com>
2022-11-07 01:01:59 +0000

    seed leaderboard and open mvc

---
+++ node-js/controllers/LeaderBoardController.ts
@@ -0,0 +1,8 @@
+import { LeaderBoardService } from "../services/LeaderBoardService";
+
+export class LeaderBoardController {
+    constructor(private leaderBoardService: LeaderBoardService){
+        this.leaderBoardService
+        // not yet do
+    }
+}
--- node-js/main.ts
+++ node-js/main.ts
@@ -3,16 +3,29 @@ dotenv.config();
 
 import express from 'express';
 import path from "path";
+import Knex from "knex";
 
+const knexConfig = require("./knexfile");
+const configMode = process.env.NODE_ENV || "development";
+const knex = Knex(knexConfig[configMode]);
 
 const app = express();
 app.use(express.json());
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname,'public')));
+
+// knex print out leaderboard
+// async function leaderboard(){
+//     const board = await knex.select('username','score')
+//                     .from('leaderboard')
+//                     .orderBy("score",'desc')
+//     console.log(board[0])
+// }
+// leaderboard()
 
 app.use((req, res) => {
     res.status(404);
-    res.sendFile(path.resolve('./public/404.html'));
+    res.sendFile(path.join(__dirname,'public','404.html'));
 });
 
 const PORT = 8080;
