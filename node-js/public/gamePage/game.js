66889b47abc19a654fed5a43e0d2668dbf55d32a
Duncan Tang <pofungt@hotmail.com>
2022-11-08 01:57:50 -0500

    Count-down of game finished

--- node-js/public/gamePage/game.js
+++ node-js/public/gamePage/game.js
@@ -1,3 +1,17 @@
+function gameStart() {
+    const countDownToStart = document.querySelector("#count_down_to_start");
+    countDownToStart.style.display = "flex";
+    setInterval(()=>{
+        countDownToStart.innerHTML = "2";
+        setInterval(()=>{
+            countDownToStart.innerHTML = "1";
+            setInterval(()=>{
+                countDownToStart.style.display = "none";
+            },1000);
+        }, 1000);
+    }, 1000);
+}
+
 function openCvReady() {
     cv['onRuntimeInitialized'] = ()=>{
         const constraints = {
