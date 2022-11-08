a2afd43fab00ec8fbd9ac6e9baf44529a2859f95
Duncan Tang <pofungt@hotmail.com>
2022-11-08 13:06:37 -0500

    Countdown update

--- node-js/public/gamePage/js/game.js
+++ node-js/public/gamePage/js/game.js
@@ -11,10 +11,10 @@ class EmoGame {
 
 async function gameStart() {
     const countDownToStart = document.querySelector("#count_down_to_start");
-    countDownToStart.style.display = "flex";
     let game = new EmoGame();
-
+    
     // Countdown to start
+    countDownToStart.style.display = "flex";
     await sleep(()=>{countDownToStart.innerHTML = "2";}, 1000);
     await sleep(()=>{countDownToStart.innerHTML = "1";}, 1000);
     await sleep(()=>{countDownToStart.style.display = "none";}, 1000);
