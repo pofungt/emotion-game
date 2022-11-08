9e0d20693d6271efe578aae626d173303d58edc7
Duncan Tang <pofungt@hotmail.com>
2022-11-08 13:05:58 -0500

    Updated countdown to promisify version

--- node-js/public/gamePage/js/game.js
+++ node-js/public/gamePage/js/game.js
@@ -1,13 +1,24 @@
-function gameStart() {
+import { sleep } from "./setTimeout.js";
+
+class EmoGame {
+    constructor() {
+        this.point = 0;
+        this.level = 1;
+        this.correctAnswers = 0;
+        this.questionsAsked = 0;
+    }
+}
+
+async function gameStart() {
     const countDownToStart = document.querySelector("#count_down_to_start");
     countDownToStart.style.display = "flex";
-    setInterval(()=>{
-        countDownToStart.innerHTML = "2";
-        setInterval(()=>{
-            countDownToStart.innerHTML = "1";
-            setInterval(()=>{
-                countDownToStart.style.display = "none";
-            },1000);
-        }, 1000);
-    }, 1000);
+    let game = new EmoGame();
+
+    // Countdown to start
+    await sleep(()=>{countDownToStart.innerHTML = "2";}, 1000);
+    await sleep(()=>{countDownToStart.innerHTML = "1";}, 1000);
+    await sleep(()=>{countDownToStart.style.display = "none";}, 1000);
+    
 }
+
+gameStart();
