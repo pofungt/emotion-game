e9f4314e0e13094aa9e09c2f9a707a5e785c0b46
Duncan Tang <pofungt@hotmail.com>
2022-11-10 12:01:40 -0500

    Done enter part of leaderboard input

--- node-js/public/resultPage/result.js
+++ node-js/public/resultPage/result.js
@@ -1,7 +1,8 @@
-window.onload = () => {
-  getLeaderBoard();
+window.onload = async () => {
+  await getLeaderBoard();
   getScore();
   callLeaderBoard();
+  submitScore();
 };
 
 let lowestScore = 0;
@@ -17,52 +18,62 @@ async function getLeaderBoard() {
         <th>Score</th>
     </tr>`;
   let writeScore = false;
-    for (const i in board){
-      if (board[i].username && i < 10){
-          let rank = parseInt(i) + 1;
-          if (score > board[i].score && !writeScore){
-            htmlStr += `
+  for (const i in board) {
+    if (board[i].username && i < 10) {
+      let rank = parseInt(i) + 1;
+      if (score > board[i].score && !writeScore) {
+        htmlStr += `
             <tr>
             <td>${rank}</td>
             <td id="your-name"><div class="input-group">
-            <input type="text" class="form-control" placeholder="Input Your Name" aria-label="Username" aria-describedby="addon-wrapping">
+            <input type="text" class="form-control" id="input-name" placeholder="Input Your Name" aria-label="Username" aria-describedby="addon-wrapping">
           </div></td>
             <td>${score}</td>
             </tr>
             `;
-            writeScore = true;
-          }  
-          if (writeScore){
-            rank += 1;
-          }
-          htmlStr += `
+        writeScore = true;
+      }
+      if (writeScore) {
+        rank += 1;
+      }
+      htmlStr += `
                 <tr>
                     <td>${rank}</td>
                     <td>${board[i].username}</td>
                     <td>${board[i].score}</td>
                 </tr>
                 `
-          lowestScore = board[i].score;
-        }
-    };
+      lowestScore = board[i].score;
+    }
+  };
   document.querySelector("#leader-board").innerHTML = htmlStr;
-  console.log(lowestScore)
 }
 
 let params = new URLSearchParams(window.location.search);
-let score = parseInt(params.get("points"),10);
-let correct = parseInt(params.get("correct"),10);
+let score = parseInt(params.get("points"), 10);
+let correct = parseInt(params.get("correct"), 10);
 
 // get score from game
-async function getScore() {
+function getScore() {
   document.querySelector('#score').innerHTML = score;
-  document.querySelector('#correct').innerHTML = `${correct}/25`
+  document.querySelector('#correct').innerHTML = `${correct}/15`
 }
 
 // call leaderboard up when score is within top ten.
-async function callLeaderBoard() {
-  if (score > lowestScore){
-    console.log("Yes you are on board")
-    document.querySelector('.btn').click();
+function callLeaderBoard() {
+  if (score > lowestScore) {
+    document.querySelector('#leaderboard_button').click();
+  }
+}
+
+// Submit score when pressed enter in 
+function submitScore() {
+  const inputBox = document.querySelector("#input-name");
+  if (inputBox) {
+    inputBox.addEventListener('keypress', function (e) {
+      if (e.key === 'Enter') {
+        console.log(inputBox.value)
+      }
+    });
   }
 }
