c1217818fe3d0ac82028019568dc7e5a1d448406
Duncan Tang <pofungt@hotmail.com>
2022-11-11 15:13:15 -0500

    changed fetch url to pythonAI

--- node-js/public/gamePage/js/cameraStreaming.js
+++ node-js/public/gamePage/js/cameraStreaming.js
@@ -36,7 +36,7 @@ function camera() {
             src.copyTo(dst);
             cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
             frame64 = getFrame();
-            const res = await fetch('https://python.duncantang.dev/stream',{
+            const res = await fetch('https://pythonAI.duncantang.dev/stream',{
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
