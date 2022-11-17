a5b76e3c3f823ca6084ee1894706a38f3b8c71a7
Duncan Tang <pofungt@hotmail.com>
2022-11-17 11:23:13 -0500

    Updated knexfile.ts

--- node-js/knexfile.ts
+++ node-js/knexfile.ts
@@ -41,10 +41,9 @@ const config: { [key: string]: Knex.Config } = {
   test: {
     client: "postgresql",
     connection: {
-      host: process.env.POSTGRES_HOST,
-      database: process.env.POSTGRES_DB,
-      user:     process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD 
+      database: process.env.TEST_DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
     },
     pool: {
       min: 2,
